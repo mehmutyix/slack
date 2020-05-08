b4f8527df3027c0eddb835f33e271e73cc31ad2d
maihemutijiang <60989189+mehmutyix@users.noreply.github.com>
2020-05-08 12:35:15 -0700

    Update CreateClass.java

--- IdeaProjects/slack/src/day_01/CreateClass.java
+++ IdeaProjects/slack/src/day_01/CreateClass.java
@@ -3,5 +3,6 @@ package day_01;
 public class CreateClass {
     public static void main(String[] args) {
         System.out.println("Today we are reviewing git");
+        // i am updating from my github
     }
 }
