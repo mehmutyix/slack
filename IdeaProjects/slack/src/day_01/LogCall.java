8a9b0eaa3f3e22e21a860ddfd4f17e7227d2f0d0
mehmutyix <mehmutyix@gmail.com>
2020-05-08 12:23:56 -0700

    add LogCall

---
+++ IdeaProjects/slack/src/day_01/LogCall.java
@@ -0,0 +1,9 @@
+package day_01;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class LogCall {
+    public static void main(String[] args) {
+        System.out.println("testing the push");
+    }
+}
