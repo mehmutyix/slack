75299c489e8c5fb430a85197f4b7e90cf67cb56a
mehmutyix <mehmutyix@gmail.com>
2020-05-08 12:39:05 -0700

    update Logcall.java

--- IdeaProjects/slack/src/day_01/LogCall.java
+++ IdeaProjects/slack/src/day_01/LogCall.java
@@ -5,5 +5,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("testing the push");
+        // testing intellij
     }
 }
